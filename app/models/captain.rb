ce734414d07dbd12aa9736de01f72f621b564068
gee2789 <geeyung.li@gmail.com>
2017-02-17 14:03:59 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,32 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications)
+    .where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    includes(boats: :classifications)
+    .where(classifications: {name: "Sailboat"})
+    .uniq
+  end
+
+  def self.motorboaters
+    includes(boats: :classifications)
+    .where(classifications: {name: "Motorboat"})
+    .uniq
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboaters.pluck(:id))
+    # "id (A)  IN (?B)" B values.  The IN is a sequel command that retrieves objects where A satisifes conditions B"
+
+  end
+
+  def self.non_sailors
+    where.not("id IN (?)", self.sailors.pluck(:id))
+
+  end
+
 end
